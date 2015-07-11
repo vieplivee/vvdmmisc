294ee8099dd0572d965231fd1f27c0671b0a6092
vieplivee <vieplivee@gmail.com>
2015-07-10 20:51:52 -0400

    add package man page

---
+++ R/vvdmmisc.R
@@ -0,0 +1,8 @@
+#' Misc stuff for data mining
+#'
+#' @name vvdmmisc
+#' @aliases vv
+#' @docType package
+#' @import data.table plyr dplyr reshape2 stringr ggplot2 lattice lubridate pROC
+#'     ROCR caret randomForest C50 unbalanced
+NULL
